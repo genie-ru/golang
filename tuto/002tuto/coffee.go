9d5bffe80c0b59d0a0aa84e3781e50613e59c2d9
genie-ru <genie.zgmf987@gmail.com>
2025-02-21 21:53:04 +0900

    test

---
+++ tuto/002tuto/coffee.go
@@ -0,0 +1,6 @@
+package main
+
+type Coffee struct {
+  Name string
+  Size string
+}
--- tuto/002tuto/main.go
+++ tuto/002tuto/main.go
@@ -1,17 +1,17 @@
 package main
-
 import "fmt"
 
 func main() {
-	emptyMap := make(map[string]int)
-	mapWithElements :=map[string]int{"a" : 1, "b" : 2}
-	fmt.Println("Maps - map1 ", emptyMap, " and map2 ", mapWithElements)
-
-	latte := Coffee{Name: "Latte", Size: 3}
-	fmt.Println("Coffee instance ", latte)
+    numbers := []int{1, 2, 3, 4, 5}
+    fmt.Printf("Printing all odd numbers %v", findAllOddNumbers(numbers))
 }
 
-type Coffee struct {
-	Name string
-	Size int
+func findAllOddNumbers(numbers []int) []int {
+  result := []int{}
+  for _, num := range numbers {
+    if num % 2 != 0 {
+      result = append(result, num)
+    }
+  }
+  return result
 }
